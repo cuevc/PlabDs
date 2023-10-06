76dc9080b480290287214e25d08ab9fa9eb8d619
josias <1603392@autonoma.cat>
2023-10-06 09:20:23 +0200

    Users roles update. Only class diagram

--- src/baseNoStates/Group.java
+++ src/baseNoStates/Group.java
@@ -9,28 +9,65 @@ public class Group {
     private Schedule schedules;
     private List<Actions> actions;
 
-    public Group(String name) {
+    public Group(String name, Schedule setSchedule, List<Actions> operativeActions) {
         typeGroup = name;
+        schedules = setSchedule;
+        actions = operativeActions;
+    }
+    public String getString() {return typeGroup;}
+    public List<User> getUsers() {return users;}
+    public Schedule getSchedules() {return schedules;}
+    public List<Actions> getActions () {return actions;}
 
-        if(typeGroup.equals("Admin") || typeGroup.equals("admin") || typeGroup.equals("ADMIN"))
+    public boolean isOnUsers(User searchUser)
+    {
+        //Looking for user with the same credential and same name
+        //if already exists then return true, otherwise false.
+        for( User actualUser:users)
         {
-            schedules.setAdmin();
-        }
-        else {
-            if (typeGroup.equals("Manager") || typeGroup.equals("manager") || typeGroup.equals("MANAGER")) {
-                schedules.setManager();
-            }
-            else{
-                if(typeGroup.equals("Employee") || typeGroup.equals("employee") || typeGroup.equals("EMPLOYEE")){
-                    schedules.setEmployee();
-                }
-                else {
-                    if(typeGroup.equals("Blank") || typeGroup.equals("blank") ||typeGroup.equals("BLANK")){
-                        schedules.setBlank();
-                    }
-                }
+            if(actualUser.getCredential() == searchUser.getCredential() &&actualUser.getName() == searchUser.getName() ){
+                return true;
             }
         }
+        return false;
+    }
+    public User searchUser(String userCredential, String userName){
+        for( User actualUser:users)
+        {
+            if(actualUser.getCredential() == userCredential &&actualUser.getName() == userName ){
+                return actualUser;
+            }
+        }
+        System.out.println("The user: " + userName + " with credential: " + userCredential + " has not been found");
+        return null;
+    }
+    public void addUser(User newUser)
+    {
+        //
+        if( !isOnUsers(newUser)) {
+            users.add(newUser);
+            System.out.println(newUser.toString() + " added succesfully");
+        }
+        else{
+            System.out.println(newUser.toString() + " is already on the list");
+        }
+    }
+    public void removeUser(User deleteUser)
+    {
+        if(isOnUsers(deleteUser)){
+            users.remove(deleteUser);
+            System.out.println(deleteUser.toString() + " remove succesfully.");
+        }
+        else{
+            System.out.println(deleteUser.toString() + " does not exist in this group.");
 
+        }
+    }
+    public boolean hasAction(Actions searchedAction)
+    {
+        if(actions.contains(searchedAction)){
+            return true;
+        }
+        return false;
     }
 }
