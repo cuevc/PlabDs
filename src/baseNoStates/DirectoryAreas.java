c620e96d2b200da76fe47aab63f185417eb56393
PolKinsa <1605612@uab.cat>
2023-10-06 17:51:56 +0200

    Working on the Composition

--- src/baseNoStates/DirectoryAreas.java
+++ src/baseNoStates/DirectoryAreas.java
@@ -1,7 +1,26 @@
 package baseNoStates;
 
+import java.util.ArrayList;
+
 public class DirectoryAreas {
+    private ArrayList<Door> allDoors;
+    private Area rootArea;
+
     public static Area findAreaById(String areaId) {
         return null;
+    }
+
+    public void makeAreas(){
+
+    }
+
+    public Door findDoorById(String id) {
+        // We search the given Door on the ArrayList of allDoors.
+        if (allDoors.contains(new Door(id))) {
+            return new Door(id);
+        }
+        else {
+            return null;
+        }
     }
 }
